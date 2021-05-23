a09638d032239fbe9c93f3f2fa3ad48f75c7ca22
PawinderGit <Pawinder@163.com>
2021-05-23 23:31:29 +0800

    Merge fixed

--- Assets/Scripts/Bombs/BombGeneratorOnline.cs
+++ Assets/Scripts/Bombs/BombGeneratorOnline.cs
@@ -5,15 +5,8 @@ using Photon.Pun;
 
 public class BombGeneratorOnline : MonoBehaviourPun
 {
-	[Tooltip("设置为false无限弹药,true会消耗弹药库存")]
-	public bool useAmmunition = false;
-
 	public float bombsFallingInterval = 10f;       // 冰壶炸弹降落间隔时间
 	public int bombsFallingNumber = 10;            // 每次掉落的冰壶炸弹数（无限弹药情况下）
-
-	int ammunitionStock = 0;
-	bool hasOwner = false;
-	Team owner;
 
 	[System.Serializable]
 	public class GeneratingArea
@@ -36,19 +29,7 @@ public class BombGeneratorOnline : MonoBehaviourPun
 	{
 		while (inGame)
 		{
-			// 生成炸弹
-			int generateNum;
-			if (useAmmunition)
-			{
-				generateNum = ammunitionStock;
-				ammunitionStock = 0;
-			}
-			else
-			{
-				generateNum = bombsFallingNumber;
-			}
-
-			for (int i = 0; i < generateNum; i++)
+			for (int i = 0; i < bombsFallingNumber; i++)
 			{
 				GenerateBomb();
 			}
@@ -57,34 +38,46 @@ public class BombGeneratorOnline : MonoBehaviourPun
 		}
 	}
 
-	void GenerateBomb()
+	GameObject GenerateBomb(string bombFileName = "DefaultBomb")
 	{
 		Vector3 position = new Vector3(
 			Random.Range(transform.position.x - generatingArea.xWidth / 2, transform.position.x + generatingArea.xWidth / 2),
 			transform.position.y,
 			Random.Range(transform.position.z - generatingArea.zWidth / 2, transform.position.z + generatingArea.zWidth / 2));
 		GameObject bomb
-		= PhotonNetwork.Instantiate("DefaultBomb", position, Quaternion.identity);
+		= PhotonNetwork.Instantiate(bombFileName, position, Quaternion.identity);
 		bomb.GetComponent<Rigidbody>().velocity = new Vector3(Random.value > 0.5 ? 1 : -1, 0, Random.value > 0.5 ? 1 : -1);
-		if (hasOwner)
+
+		return bomb;
+	}
+
+	public void AddBombAndGenerate(Team owner, int number)
+	{
+		string resouceName;
+		switch (owner)
 		{
-			bomb.GetComponent<Bomb>().AddAlly(owner);
+			case Team.Blue:
+				resouceName = "BlueBomb";
+				break;
+			case Team.Red:
+				resouceName = "RedBomb";
+				break;
+			case Team.Yellow:
+				resouceName = "YellowBomb";
+				break;
+			case Team.Green:
+				resouceName = "GreenBomb";
+				break;
+			default:
+				resouceName = "";
+				break;
 		}
-	}
 
-	public void AddBomb(int num = 1)
-	{
-		ammunitionStock += num;
-	}
-
-	/// <summary>
-	/// Set owner Team
-	/// </summary>
-	/// <param name="team"></param>
-	public void SetOwner(Team team)
-	{
-		hasOwner = true;
-		owner = team;
+		for (int i = 0; i < number; i++)
+		{
+			GameObject bombObject = GenerateBomb(resouceName);
+			bombObject.GetComponent<Bomb>().AddAlly(owner);
+		}
 	}
 
 	private void OnDrawGizmosSelected()
